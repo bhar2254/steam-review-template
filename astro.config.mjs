dbc5a2143d99bb924878baf9c5da40decde0a660
Blaine Harper <bhar2254@gmail.com>
2026-06-24 12:36:31 -0500

    fixing formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,10 @@ import tailwind from "@astrojs/tailwind";
 
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
+// astro.config.mjs
 export default defineConfig({
-  site: "https://bhar2254.github.io/",
-  base: "/steam-review-template",
+  site: "https://bhar2254.github.io",
+  base: "/steam-review-template/", 
   integrations: [tailwind(), sitemap()],
   i18n: {
     defaultLocale: "en",
